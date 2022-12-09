694f79e926aa201aedb3465a9ba5763b6f50ca1c
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-09 11:47:23 +0530

    fetching userid

--- src/main/java/com/example/shoppingcart_backend/dao/CustomersDao.java
+++ src/main/java/com/example/shoppingcart_backend/dao/CustomersDao.java
@@ -11,4 +11,7 @@ public interface CustomersDao extends CrudRepository<Customers,Integer> {
 
     @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phno`, `username` FROM `customer` WHERE `username`=:username AND`password`=:password",nativeQuery = true)
     List<Customers> login(@Param("username") String username,@Param("password") String password);
+
+    @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phno`, `username` FROM `customer` WHERE `id`=:id",nativeQuery = true)
+    List<Customers> profileview(@Param("id") Integer id);
 }
